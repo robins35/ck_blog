337d52e528d3e49d3bb98c3144929eef1064b031
robins35 <caiden.robinson35@gmail.com>
2015-11-22 17:18:57 -0800

    Fully installed Devise

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,4 +1,4 @@
-class Devise::PasswordsController < Devise::PasswordsController
+class Users::PasswordsController < Devise::PasswordsController
   # GET /resource/password/new
   # def new
   #   super
